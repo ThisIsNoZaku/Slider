9c3211492b9130fca7e94411d6174f10eb971d26
Damie <damienmarble@gmail.com>
2019-05-11 23:27:28 -0400

    Initial commit.

---
+++ core/src/com/thisisnozaku/resources/StockResource.java
@@ -0,0 +1,7 @@
+package com.thisisnozaku.resources;
+
+/**
+ * A resource which can be spent and accumulates.
+ */
+public class StockResource {
+}
---
+++ desktop/src/com/thisisnozaku/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.thisisnozaku.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.thisisnozaku.CivCombiner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CivCombiner(), config);
+	}
+}
